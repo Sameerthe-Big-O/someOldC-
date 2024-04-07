3e83fcbe6dcf45975e050ac62b9d2d65e641b12d
Sameerthe-Big-O <diziaddict00@gmail.com>
2024-04-07 18:12:56 +0500

    something something is up

--- basic.cpp
+++ basic.cpp
@@ -18,29 +18,29 @@ int main() {
     // cin >> num>>nums;   // Taking input
     // cout << "The number is:"<< num+nums; 
      // assigning an int value to num_int
-//   int num_int = 9;
+  int num_it = 9;
 
-//   // declaring a double type variable
-//   double num_double;
+  // declaring a double type variable
+  double num_dou;
  
-//   // implicit conversion
-//   // assigning int value to a double variable
-//   num_double = num_int;
+  //*implicit conversion
+ //*assigning int value to a double variable
+   num_dou = num_it;
 
-//   cout << "num_int = " << num_int << endl;
-//   cout << "num_double = " << num_double << endl;
+   cout << "num_it = " << num_it << endl;
+  cout << "num_double = " << num_dou << endl;
 //*implicit
-//     int num_int;
-//   double num_double = 9.99;
+  int num_in;
+  double num_doubl = 9.99;
 
-//   // implicit conversion
-//   // assigning a double value to an int variable
-//   num_int = num_double;
+  // implicit conversion
+  // assigning a double value to an int variable
+  num_in = num_doubl;
 
-//   cout << "num_int = " << num_int << endl;
-//   cout << "num_double = " << num_double << endl;
+  cout << "num_int = " << num_in << endl;
+  cout << "num_double = " << num_doubl << endl;
 
-    // initializing a double variable
+   //* initializing a double variable and type casting
     double num_double = 3.56;
     cout << "num_double = " << num_double << endl;
 
